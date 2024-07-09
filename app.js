c05d9baa1d978aea1874b058b4ce55fb00440b62
Junghwa <jungh150@naver.com>
2024-07-10 03:40:38 +0900

    add post request to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import posts from './data/mock.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/posts', (req, res) => {
     res.send(posts);
@@ -15,6 +16,19 @@ app.get('/posts/:id', (req, res) => {
     } else {
         res.status(404).send({ message: 'Cannot find given id.' });
     }
+});
+
+app.post('/posts', (req, res) => {
+    const newPost = req.body;
+    const ids = posts.map((post) => post.id);
+    newPost.id = Math.max(...ids) + 1;
+    newPost.isliked = false;
+    newPost.comments = {};
+    newPost.createdAt = new Date();
+    newPost.updatedAt = new Date();
+
+    posts.push(newPost);
+    res.status(201).send(newPost);
 });
 
 app.listen(3000, () => console.log('Server Started'));
