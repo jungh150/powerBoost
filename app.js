c0a68dd147f436bf9cbc71ec606074933c2eb186
Junghwa <jungh150@naver.com>
2024-07-10 03:36:44 +0900

    add get request to app.js

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import posts from './data/mock.js';
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello Express!');
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.get('/posts/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        res.send(post);
+    } else {
+        res.status(404).send({ message: 'Cannot find given id.' });
+    }
 });
 
 app.listen(3000, () => console.log('Server Started'));
