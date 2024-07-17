2853dcbf64f7dc67960cd009707ff4d03c708f52
Junghwa <jungh150@naver.com>
2024-07-17 17:53:07 +0900

    create user api

--- app.js
+++ app.js
@@ -3,76 +3,126 @@ dotenv.config();
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
 const app = express();
 app.use(express.json());
 
+/*********** users ***********/
+
+// 전체 유저 조회
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
+// 특정 유저 조회
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+  res.send(user);
+});
+
+// 유저 생성
+app.post('/users', async (req, res) => {
+  const user = await prisma.user.create({
+    data: req.body,
+  });
+  res.status(201).send(user);
+});
+
+// 유저 수정
+app.patch('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(user);
+});
+
+// 유저 삭제
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: { id },
+  });
+  res.sendStatus(204);
+});
+
+/*********** posts ***********/
+
 // 전체 글 조회
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 // 특정 글 조회
 app.get('/posts/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        res.send(post);
-    } else {
-        res.status(404).send({ message: 'Cannot find given id.' });
-    }
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    res.send(post);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
 });
 
 // 글 작성
 app.post('/posts', (req, res) => {
-    const newPost = req.body;
-    const ids = posts.map((post) => post.id);
-    newPost.id = Math.max(...ids) + 1;
-    newPost.isliked = false;
-    newPost.comments = [];
-    newPost.createdAt = new Date();
-    newPost.updatedAt = new Date();
+  const newPost = req.body;
+  const ids = posts.map((post) => post.id);
+  newPost.id = Math.max(...ids) + 1;
+  newPost.isliked = false;
+  newPost.comments = [];
+  newPost.createdAt = new Date();
+  newPost.updatedAt = new Date();
 
-    posts.push(newPost);
-    res.status(201).send(newPost);
+  posts.push(newPost);
+  res.status(201).send(newPost);
 });
 
 // 글 수정 (글에 좋아요 달기)
 app.patch('/posts/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        Object.keys(req.body).forEach((key) => {
-            post[key] = req.body[key];
-        });
-        post.updatedAt = new Date();
-        res.send(post);
-    } else {
-        res.status(404).send({ message: 'Cannot find given id.' });
-    }
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    Object.keys(req.body).forEach((key) => {
+      post[key] = req.body[key];
+    });
+    post.updatedAt = new Date();
+    res.send(post);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
 });
 
 // 글에 댓글 달기
 app.patch('/comments/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        post['comments'].push(req.body['comment']);
-        res.send(post);
-    } else {
-        res.status(404).send({ message: 'Cannot find given id.' });
-    }
+  const id = Number(req.params.id);
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    post['comments'].push(req.body['comment']);
+    res.send(post);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
 });
 
 // 글 삭제
 app.delete('/posts/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const idx = posts.findIndex((post) => post.id === id);
-    if (idx >= 0) {
-        posts.splice(idx, 1);
-        res.sendStatus(204);
-    } else {
-        res.status(404).send({ message: 'Cannot find given id.' });
-    }
+  const id = Number(req.params.id);
+  const idx = posts.findIndex((post) => post.id === id);
+  if (idx >= 0) {
+    posts.splice(idx, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).send({ message: 'Cannot find given id.' });
+  }
 });
+
+/*********** commets ***********/
 
 app.listen(3000, () => console.log('Server Started'));
